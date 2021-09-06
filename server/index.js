d5d68946f92cbd9bec650b2db4df9710f01d4666
prashanthy <tomu644@gmail.com>
2021-09-06 02:34:36 -0700

    Node Server side CRUD

--- server/index.js
+++ server/index.js
@@ -2,5 +2,23 @@ import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from 'cors'
+import postRoutes from './routes/posts.js'
 
-const express = require('express')
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors());
+
+
+const CONNECTION_URL = "mongodb+srv://yuvaanmastery:yuvaanmastery123@cluster0.hzeee.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`)))
+    .catch((err) => console.log(err.message));
+
+
+// mongoose.set("useFindAndModify", false);
