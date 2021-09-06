72a986c7e59a15fac9451aa3694269f0e4fbbe07
prashanthy <tomu644@gmail.com>
2021-09-06 03:21:55 -0700

    h

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors());
 
 
-const CONNECTION_URL = "mongodb+srv://yuvaanmastery:yuvaanmastery123@cluster0.hzeee.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://yuvaanmastery:<pwd>@cluster0.hzeee.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
