42551bbddf3854ea618304c3a0d7ffd5ff5ef56f
prashanthy <tomu644@gmail.com>
2021-10-03 21:02:46 -0700

    More client side changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import memories from './images/memories.png'
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">
+                    Memories
+                    <img src={memories} alt="memories" height="60" />
+                </Typography>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={4}>
+                        <Grid item xs={12} sm={7}>
+                            
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
